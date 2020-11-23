f90e98a2df3cb361b16b7aeb1ed1a357290cf459
Abhishek <abhishekm942@gmail.com>
2020-11-24 04:43:28 +0530

    Initial commit

---
+++ Assets/Editor/ShapeEditor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShapeCreator))]
+public class ShapeEditor : Editor
+{
+    ShapeCreator shapeCreator;
+    bool needsRepaint;
+
+    private void OnSceneGUI()
+    {
+        Event guiEvent = Event.current;
+
+        if (guiEvent.type == EventType.Repaint)
+        {
+            Draw();
+        }
+        else if (guiEvent.type == EventType.Layout)
+        {
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+        }
+        else
+        {
+            HandleInput(guiEvent);
+
+            if (needsRepaint)
+            {
+                HandleUtility.Repaint();
+            }
+        }
+    }
+
+    void HandleInput(Event guiEvent)
+    {
+        Ray ray = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
+        float drawPlaneHeight = 0;
+        float distToDrawPlane = (drawPlaneHeight - ray.origin.y) / ray.direction.y;
+        Vector3 mousePosition = ray.GetPoint(distToDrawPlane);
+
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.modifiers == EventModifiers.None)
+        {
+            Undo.RecordObject(shapeCreator, "Add point");
+            shapeCreator.points.Add(mousePosition);
+            Debug.Log(mousePosition);
+            needsRepaint = true;
+        }
+    }
+
+    void Draw()
+    {
+        for (int i = 0; i < shapeCreator.points.Count; i++)
+        {
+            Vector3 nextPoint = shapeCreator.points[(i + 1) % shapeCreator.points.Count];
+            Handles.color = Color.black;
+            Handles.DrawDottedLine(shapeCreator.points[i], nextPoint, 4f);
+            Handles.color = Color.white;
+            Handles.DrawSolidDisc(shapeCreator.points[i], Vector3.up, 0.5f);
+        }
+        needsRepaint = false;
+    }
+
+    private void OnEnable()
+    {
+        shapeCreator = (ShapeCreator)target;
+    }
+}
